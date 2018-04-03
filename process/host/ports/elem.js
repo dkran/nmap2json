7c62beb7f57391b9cceee193aaf3dea7bb387318
Daryl Kranec <dkranec@gmail.com>
2018-04-03 10:05:51 -0400

    tweaked elem a little

--- process/host/ports/elem.js
+++ process/host/ports/elem.js
@@ -1,14 +1,12 @@
 const processElem = (elem)=>{
   var newElem = null
   for(i = 0; i < elem.length; i++){
-    console.log(typeOf(elem[i]))
-    if(typeOf(elem[i]) === 'Object'){
+    if(typeof(elem[i]) === 'object'){
       newElem = {}
       newElem[elem[i].$.key] = elem[i]._
     }
-    if(typeOf(elem[i]) === 'String'){
-      newElem = {}
-      newElem.extradata = elem[i]
+    if(typeof(elem[i]) === 'string'){
+      newElem = elem[i]
     }
   }
   return newElem
