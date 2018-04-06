a6d2454b5bde2fbdd99fd940f29421c39fe7cbce
Daryl Kranec <dkranec@gmail.com>
2018-04-06 17:27:27 -0400

    trying to fix promises

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const xml2js = require('xml2js'),
   inspect = require('util').inspect,
   processor = require('./process');
 
-var parseNmap = (unparsedXml)=>{
+var parseNmap = (unparsedXml) => {
   if (unparsedXml) {
-    var ipInfo = {}
-    var parser = new xml2js.Parser()
-    parser.parseString(unparsedXml, (err, result) => {
-      return new Promise((resolve, reject)=>{
-        if(err) reject(err)
+    return new Promise((resolve, reject) => {
+      var ipInfo = {}
+      var parser = new xml2js.Parser()
+      parser.parseString(unparsedXml, (err, result) => {
+        if (err) reject(err)
         result = processor(result)
-        if(result) resolve(result)
+        if (result) resolve(result)
       })
       //console.log(inspect(processor(result.nmaprun), false, 30))
       //console.log(inspect(result.nmaprun[base][0], false, 20))   
