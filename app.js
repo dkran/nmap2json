6f307e265ef4c5dd7f56929ba2600a77c38bffea
Daryl Kranec <dkranec@gmail.com>
2018-04-06 20:51:25 -0400

    Went back to callbacks... this wasn't handling promises easily

--- app.js
+++ app.js
@@ -2,18 +2,14 @@ const xml2js = require('xml2js'),
   inspect = require('util').inspect,
   processor = require('./process');
 
-var parseNmap = (unparsedXml) => {
+var parseNmap = (unparsedXml, cb) => {
   if (unparsedXml) {
-    return new Promise((resolve, reject) => {
-      var ipInfo = {}
-      var parser = new xml2js.Parser()
-      parser.parseString(unparsedXml, (err, result) => {
-        if (err) reject(err)
-        result = processor(result)
-        if (result) resolve(result)
-      })
-      //console.log(inspect(processor(result.nmaprun), false, 30))
-      //console.log(inspect(result.nmaprun[base][0], false, 20))   
+    var ipInfo = {}
+    var parser = new xml2js.Parser()
+    parser.parseString(unparsedXml, (err, result) => {
+      if (err) cb(err)
+      result = processor(result)
+      if (result) cb(null, result)
     })
   }
 }
