92c9ec6290c4bdfadd5861bdf2ec39a930703557
Daryl Kranec <dkranec@gmail.com>
2018-04-19 18:55:52 -0400

    oops.

--- process/host/ports/script.js
+++ process/host/ports/script.js
@@ -1,11 +1,14 @@
 const inspect = require('util').inspect,
-  _ = require('lodash')
+  _ = require('lodash'),
+  processElem = require('./elem')
 
 const processScript = (script) => {
   var newScript = {}
-  _.forIn(script, (el, key)=>{
+  _.forIn(script, (el, num)=>{
     _.forIn(el, (el, key)=>{
       if(key === '$' && el.id) newScript[el.id] = el.output
+      //if(key === 'elem') newScript = Object.assign(processElem(el))
+      //console.log(key)
     })
   })
   return newScript
